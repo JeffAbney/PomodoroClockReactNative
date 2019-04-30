eb8e2f5d01bda498e7d5be10e5396a0baafaf987
JeffAbney <jeffmabney@gmail.com>
2019-04-30 15:44:30 -0500

    Add authentication stack

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
 
 import MainDrawerNavigator from './MainDrawerNavigator';
+import LogInScreen from '../screens/LogInScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+
+const AuthStack = createStackNavigator({ LogIn: LogInScreen, SignUp: SignUpScreen });
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+  Auth: AuthStack,
   Main: MainDrawerNavigator,
 }));
