1a8e4d55c6d8e4476b1452f14aa90549b72d6f8c
JeffAbney <jeffmabney@gmail.com>
2019-05-03 11:26:54 -0500

    Add fcuntion to get list of Task Names and Times

--- screens/ActivityLogScreen.js
+++ screens/ActivityLogScreen.js
@@ -3,6 +3,7 @@ import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import DrawerMenu from '../components/DrawerMenu';
 
+
 class activityLogScreen extends React.Component {
 
   static navigationOptions = {
@@ -19,6 +20,7 @@ class activityLogScreen extends React.Component {
 
     this.logDisplay = this.logDisplay.bind(this);
     this.getLog = this.getLog.bind(this);
+    this.categoryTime = this.categoryTime.bind(this);
   }
 
   getLog() {
@@ -57,25 +59,65 @@ class activityLogScreen extends React.Component {
     })
   }
 
+  categoryTime(cat) {
+    let arr = this.state.log;
+    let category = arr.filter((act) => act.activityCategory === cat
+    )
+    let catTime = 0;
+    let getCatTime = function () {
+      category.forEach((act) => catTime += act.activityTime);
+      console.log(catTime);
+      return catTime;
+    }
+    let getTasksWithTimes = function () {
+      let taskNames = [];
+      category.forEach((act) => {
+        if (!taskNames.includes(act.activityName)) {
+          taskNames.push(act.activityName);
+        };
+      })
+      console.log(taskNames);
+      let tasksWithTimes =[];
+      let getTimes = function () {
+      if (taskNames.length !== 0) {
+        let taskTime = 0;
+        for (i=0; i<taskNames.length; i++) {
+          let taskName = taskNames[i];
+          for (j=0; j<category.length; j++) {
+            category[j].activityName === taskName ? taskTime += category[j].activityTime : ""
+          }
+          tasksWithTimes.push([taskName, taskTime]);
+          taskTime = 0;
+        }
+        console.log("Tasks with times", tasksWithTimes)
+      } 
+    }
+    getTimes()
+    };
+    
+    return `categoryTime: ${getCatTime()}, tasks: ${getTasksWithTimes()}`  
+  }
+
+
   render() {
-    let loggedIn = this.props.screenProps.isLoggedIn;
-    let styles = this.props.screenProps.styles;
+    let { isLoggedIn, styles, username } = this.props.screenProps;
     const { navigation } = this.props;
-    if (loggedIn) {
-      if (this.props.screenProps.username != this.state.username) {
+    if (isLoggedIn) {
+      if (username != this.state.username) {
         this.getLog();
       }
       return (
         <ScrollView style={[styles.container, styles.paddingTop]}>
-        <DrawerMenu navigation={navigation} styles={styles}/>
+          <DrawerMenu navigation={navigation} styles={styles} />
           <Text style={styles.text}>Logged in as: {this.state.username}</Text>
+          <Text style={styles.text}>Data {this.categoryTime("Planning")}</Text>
           {this.logDisplay()}
         </ScrollView>
       );
     } else {
       return (
         <View style={styles.container}>
-        <DrawerMenu navigation={navigation} styles={styles}/>
+          <DrawerMenu navigation={navigation} styles={styles} />
           <Text style={styles.welcomeText}>Please Log In to view Activity Log</Text>
         </View>
       )
