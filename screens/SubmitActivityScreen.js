d645affb759ac2539084cd9b2fa56343ac9f5196
JeffAbney <jeffmabney@gmail.com>
2019-05-27 21:11:38 -0500

    Make fit new schema

--- screens/SubmitActivityScreen.js
+++ screens/SubmitActivityScreen.js
@@ -5,9 +5,12 @@ import {
   Alert,
   Button,
   View,
-  Picker
+  Picker,
+  TouchableHighlight
 } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+
+let projArr = [];
 
 export default class SubmitActivityScreen extends Component {
   static navigationOptions = {
@@ -17,123 +20,112 @@ export default class SubmitActivityScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activityCategoryList: ["Planning", "Coding", "Running"],
-      activityCategory: "Planning",
-      activityName: ""
+      projectName: projArr[0],
+      taskName: ""
     }
 
-    this._onChangeActivityCategory = this._onChangeActivityCategory.bind(this);
-    this._onChangeActivityName = this._onChangeActivityName.bind(this);
-    this._onSubmitActivity = this._onSubmitActivity.bind(this);
-    this.activityCategoryList = this.activityCategoryList.bind(this);
-    this.fetchCategoryList = this.fetchCategoryList.bind(this);
+    this._onChangeprojectName = this._onChangeprojectName.bind(this);
+    this._onChangetaskName = this._onChangetaskName.bind(this);
+    this.onSubmitTask = this.onSubmitTask.bind(this);
+    this.displayProjectList = this.displayProjectList.bind(this);
   }
 
-  componentDidMount(){
-    this.fetchCategoryList()
-  };
 
-  fetchCategoryList() {
-    let userID = this.props.screenProps.userID
-    fetch('http://localhost:3000/showLog', {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        userID: userID,
-      })
-    })
-      .then(res => {
-        return res.json()
-      })
-      .then(res => {
-        if (res.activityCategoryList != null) {
-          this.setState({
-            activityCategoryList: res.activityCategoryList
-          })
-        }
-        
-      })
-  } 
-
-
-
-  _onChangeActivityCategory(itemValue) {
-    this.setState({ activityCategory: itemValue })
+  _onChangeprojectName(itemValue) {
+    this.setState({ projectName: itemValue })
   }
 
-  _onChangeActivityName(name) {
-    this.setState({ activityName: name })
+  _onChangetaskName(name) {
+    this.setState({ taskName: name })
   }
 
-  _onSubmitActivity() {
+  onSubmitTask() {
     const { navigation } = this.props;
-    const { userID } = this.props.screenProps
-    const taskTime = navigation.getParam('activityTime', 15);
+    const { projectName, taskName } = this.state
+    const { userID, sessionTime, getProjects, userProjects } = this.props.screenProps;
+    let taskTime = Math.round(sessionTime / 60);
 
-    let { activityCategory, activityName } = this.state;
-    fetch('http://localhost:3000/log', {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        userID: userID,
-        projectName: projectName,
-        taskName: taskName,
+    if (projectName === undefined) {
+      this.props.navigation.navigate('SubmitActivity', {
+        loggedIn: true,
         taskTime: taskTime,
-        date: new Date()
       })
-    })
-      .then(res => {
-        return res.text()
+    } else {
+      fetch('http://localhost:3000/log', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userID: userID,
+          projectName: projectName,
+          taskName: taskName,
+          taskTime: taskTime,
+          date: new Date()
+        })
       })
-      .then(res => {
-        console.log(res);
-        if (res == "OK") {
-          this.props.navigation.navigate('Home', {
-            loggedIn: true,
-            username: username
-          })
-        } else {
-          Alert.alert("Error",
-            "There was a problem saving your activity",
-            [{ text: "OK" }]);
-          this.props.navigation.navigate('Home', {
-            loggedIn: true,
-            username: username
-          })
-        }
-      })
+        .then(res => {
+          return res.text()
+        })
+        .then(res => {
+          getProjects();
+          return res;
+        })
+        .then(res => {
+          console.log("clock res", res);
+          if (res === "OK") {
+            navigation.navigate('Tasks', {
+              projectName: projectName,
+              userID: userID,
+              projectLog: userProjects[projectName].log
+            })
+          } else {
+            console.log("Reached clock error")
+            Alert.alert("Error",
+              "There was a problem saving your activity",
+              [{ text: "OK" }]);
+            navigation.navigate('Home')
+          }
+        })
+    }
   }
 
-  activityCategoryList() {
-    return this.state.activityCategoryList.map((cat) => {
-      return (
-        <Picker.Item key={cat} label={cat} value={cat} />
-      )
-    }
 
-    )}
+
+  displayProjectList() {
+    let { userProjects } = this.props.screenProps;
+    projArr = [];
+    for (const proj in userProjects) {
+      if (userProjects.hasOwnProperty(proj)) {
+        projArr.push(proj)
+      }
+    }
+    console.log('SAS - projArr', projArr)
+    return (
+      projArr.map((proj) => <Picker.Item key={proj} label={proj} value={proj} />)
+    )
+
+  }
 
   render() {
     return (
       <View style={[styles.container, styles.center]}>
-        <Text>Activity Category</Text>
+        <Text style={{ marginTop: 50 }}>Activity Category</Text>
         <Picker
-          selectedValue={this.state.activityCategory}
+          selectedValue={this.state.projectName}
           style={{ height: 50, width: 300 }}
-          onValueChange={this._onChangeActivityCategory}
+          onValueChange={this._onChangeprojectName}
         >
-          {this.activityCategoryList()}
+          {this.displayProjectList()}
         </Picker>
         <Text>Activity Name</Text>
         <TextInput
           style={styles.userInput}
-          onChangeText={this._onChangeActivityName}
+          onChangeText={this._onChangetaskName}
         />
-        <Button title="Submit" onPress={this._onSubmitActivity} />
+        <TouchableHighlight style={styles.button} onPress={this.onSubmitTask}>
+          <Text style={styles.buttonText}>Submit Task</Text>
+        </TouchableHighlight>
       </View>
     )
 
